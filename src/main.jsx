12dba6ff06d4a582c4fe391022f664e8e20c927b
Rupom <rakib.t.rupom@gmail.com>
2024-11-03 19:37:08 +0600

    practice avoid duplication in state

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import TravelPractice from "./components/TravelPractice.jsx";
+import PracticeTravelEditable from "./components/PracticeTravelEditable.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <PracticeTravelEditable />
+  </React.StrictMode>
 );
