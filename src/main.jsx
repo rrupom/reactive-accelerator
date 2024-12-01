5bd6764fd9a29f3b5c8f0197c50484f60a27ab4a
Rupom <rakib.t.rupom@gmail.com>
2024-12-01 17:06:55 +0600

    challenge-4 practiced

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
+import Messenger from "./components/challenge-4/Messenger.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Messenger />
+  </React.StrictMode>
 );
